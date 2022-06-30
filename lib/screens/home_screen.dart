060a876346a40fcc3661e1bb0b8ef6aab7e76bae
Jannis <jannismueller2001@gmail.com>
2022-06-30 15:38:21 +0200

    added drawer with method to fill ListTiles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -35,9 +35,26 @@ class _HomeScreenState extends State<HomeScreen> {
               icon: Icon(Icons.logout)),
         ],
       ),
-      drawer: Container(
-        color: kBackgroundColor,
-        width: 250,
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        backgroundColor: kBackgroundColor,
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: kMainColor,
+              ),
+              child: Image(
+                image: AssetImage('assets/icon_2500px.png'),
+              ),
+            ),
+            ...addListTilesToDrawer()
+          ],
+        ),
       ),
       body: WelcomeScreen(),
       bottomNavigationBar: BottomAppBar(
@@ -83,5 +100,30 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+
+  List<Widget> addListTilesToDrawer(){
+    final listTiles = <Widget>[];
+
+    listTiles.add(
+        ListTile(
+          title: const Text('Item 1'),
+          onTap: () {
+            // Update the state of the app.
+            // ...
+          },
+        )
+    );
+
+    listTiles.add(
+        ListTile(
+          title: const Text('Item 3'),
+          onTap: () {
+            // Update the state of the app.
+            // ...
+          },
+        )
+    );
+    return listTiles;
   }
 }
