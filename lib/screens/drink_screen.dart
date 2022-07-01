27778fce5b03fd088832463de518d84e0a601dec
Jannis <jannismueller2001@gmail.com>
2022-07-01 18:06:31 +0200

    load drinks from database

--- lib/screens/drink_screen.dart
+++ lib/screens/drink_screen.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:developer' as developer;
 import 'package:dpsg_app/shared/colors.dart';
 import 'package:dpsg_app/shared/custom_app_bar.dart';
 import 'package:dpsg_app/shared/custom_bottom_bar.dart';
 import 'package:dpsg_app/shared/custom_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 class DrinkScreen extends StatefulWidget {
   const DrinkScreen({Key? key}) : super(key: key);
@@ -14,53 +19,64 @@ class DrinkScreen extends StatefulWidget {
 class _DrinkScreenState extends State<DrinkScreen> {
   @override
   Widget build(BuildContext context) {
-    List<Widget> drinkCards = [];
-    drinks.forEach(
-      (element) {
-        drinkCards.add(
-          MaterialButton(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(22)),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                element.icon,
-                Text(
-                  element.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  element.price.toStringAsFixed(2).replaceAll('.', ',') + " €",
-                  style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            onPressed: (() {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return BuyDialog(element);
-                  });
-            }),
-            color: kMainColor,
-          ),
-        );
-      },
-    );
     return Scaffold(
       appBar: CustomAppBar(appBarTitle: "Getränke"),
       drawer: CustomDrawer(),
-      body: GridView.count(
-        padding: const EdgeInsets.all(6),
-        crossAxisSpacing: 12,
-        mainAxisSpacing: 12,
-        crossAxisCount: 2,
-        children: drinkCards,
+      body: FutureBuilder<List<Drink>>(
+          future: fetchDrinks(),
+          builder: (context, AsyncSnapshot<List<Drink>> snapshot) {
+            if (snapshot.hasData) {
+              List<Widget> drinkCards = [];
+              snapshot.data!.forEach(
+                    (element) {
+                  drinkCards.add(
+                    MaterialButton(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(22)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          element.icon,
+                          Text(
+                            element.name,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            element.price.toStringAsFixed(2).replaceAll('.', ',') + " €",
+                            style: const TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      onPressed: (() {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return BuyDialog(element);
+                            });
+                      }),
+                      color: kMainColor,
+                    ),
+                  );
+                },
+              );
+              return GridView.count(
+                padding: const EdgeInsets.all(6),
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                crossAxisCount: 2,
+                children: drinkCards,
+              );
+            } else {
+              return Center(
+                child:  CircularProgressIndicator()
+              );
+            }
+          }
       ),
       backgroundColor: kBackgroundColor,
       bottomNavigationBar: CustomBottomBar(),
@@ -74,6 +90,53 @@ class _DrinkScreenState extends State<DrinkScreen> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+
+  }
+
+  Future<List<Drink>> fetchDrinks() async {
+    //load files
+    final directory = await getApplicationDocumentsDirectory();
+    final path = await directory.path;
+    final loginFile = await File('$path/loginInformation.txt');
+    final drinksFile = await File('$path/drinks.txt');
+
+    //try to fetch data from server
+    try {
+      final loginInformation = jsonDecode(await loginFile.readAsString());
+      final token = loginInformation['token'];
+      final response = await http.get(
+          Uri.parse('http://api.dpsg-gladbach.de:3000/api/drink'),
+          headers: <String, String>{
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token'
+          }
+      );
+      developer.log(response.statusCode.toString());
+      if(response.statusCode == 200){
+        //save new data at local storage if successful
+        drinksFile.writeAsString(response.body);
+      }
+    } catch (e) {
+      developer.log(e.toString());
+    }
+
+    //load drinks from local storage
+    final drinksString = await drinksFile.readAsString();
+    final drinksJson = await jsonDecode(drinksString);
+
+    List<Drink> drinks = [];
+    drinksJson.forEach((drink) =>
+        drinks.add(
+        new Drink(
+            id: drink['id'].toInt(),
+            name: drink['name'].toString(),
+            icon: Icon(Icons.local_drink),
+            price: drink['cost'].toDouble()
+        ))
+
+    );
+
+    return Future<List<Drink>>.value(drinks);
   }
 }
 
@@ -177,7 +240,7 @@ class Drink {
       required this.price});
 }
 
-List<Drink> drinks = [
+List<Drink> drinksExample = [
   Drink(
     id: 1,
     name: "Vulkan Pils",
